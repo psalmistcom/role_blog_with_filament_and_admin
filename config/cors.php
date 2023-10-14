6e047baed6244ef568546abbcdeb68d75e5c0444
psalmistcom <dadewale9@gmail.com>
2023-10-14 13:51:21 +0100

    text widget migration

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost', 'http://localhost/storage', '*'],
 
     'allowed_origins_patterns' => [],
 
