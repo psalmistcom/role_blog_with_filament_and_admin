312aa38e8d246a5ffab5ab0d90f812f0e94628c3
psalmistcom <dadewale9@gmail.com>
2023-11-13 17:47:01 +0100

    meta fields migration added

---
+++ database/migrations/2023_11_13_164434_add_meta_fields_to_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('meta_title')->nullable();
+            $table->string('meta_description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+};
