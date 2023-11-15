a958fbe6a57a0ccf0e058749620e80d8736fb26b
psalmistcom <dadewale9@gmail.com>
2023-11-15 14:28:35 +0100

    created admin seed role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +16,19 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\Post::factory(50)->create();
-        \App\Models\User::factory(1)->create();
+        // \App\Models\User::factory(1)->create();
+
+        /** @var \App\Models\User $adminUser */
+
+        $adminUser = User::factory()->create([
+            'email' => 'admin@yopmail.com',
+            'name' => 'Admin Yop',
+            'password' => bcrypt('password')
+        ]);
+
+        $adminRole = Role::create(['name' => 'admin']);
+
+        $adminUser->assignRole($adminRole);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
