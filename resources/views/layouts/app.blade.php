7fde41e1c3ef46b9b6795bd94d0f6bc328e76d70
psalmistcom <dadewale9@gmail.com>
2023-11-15 13:58:57 +0100

    added breeze and patch some bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,29 +8,15 @@
     <meta name="author" content="HighQ Innovations">
     <meta name="description" content="{{ $metaDescription }}">
 
-    <!-- Tailwind -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
-
-        .font-family-karla {
-            font-family: karla;
-        }
-
-        pre {
-            padding: 1rem;
-            background: #1a202c;
-            color: #ffffff;
-            border-radius: 0.5rem;
-            margin-bottom: 1rem;
-        }
     </style>
 
-    <!-- AlpineJS -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
         integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-gray-50 font-family-karla">
@@ -58,15 +44,64 @@
         </div>
         <div :class="open ? 'block' : 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
             <div
-                class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-                <a href="{{ route('home') }}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Home</a>
-                @foreach ($categories as $category)
-                    <a href="{{ route('by-category', $category) }}"
-                        class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2 {{ request('category')?->slug === $category->slug ? 'bg-gray-300' : '' }}">{{ $category->title }}</a>
-                @endforeach
-                <a href="{{ route('about-us') }}"
-                    class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About
-                    us</a>
+                class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-between text-sm font-bold uppercase mt-0 px-6 py-2">
+                <div>
+                    <a href="{{ route('home') }}"
+                        class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Home</a>
+                    @foreach ($categories as $category)
+                        <a href="{{ route('by-category', $category) }}"
+                            class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2 {{ request('category')?->slug === $category->slug ? 'bg-gray-300' : '' }}">{{ $category->title }}</a>
+                    @endforeach
+                    <a href="{{ route('about-us') }}"
+                        class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About us</a>
+                </div>
+
+                <div>
+                    @auth
+                        <div class="flex sm:items-center sm:ms-6">
+                            <x-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <button class="inline-flex hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">
+                                        <div style="text-transform: capitalize">
+                                            Welcome
+                                            {{ Auth::user()->name }}</div>
+
+                                        <div class="ms-1">
+                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </x-slot>
+
+                                <x-slot name="content">
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Profile') }}
+                                    </x-dropdown-link>
+
+                                    <!-- Authentication -->
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+
+                                        <x-dropdown-link :href="route('logout')"
+                                            onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                                            {{ __('Log Out') }}
+                                        </x-dropdown-link>
+                                    </form>
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
+                    @else
+                        <a href="{{ route('register') }}"
+                            class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2 text-white bg-slate-900 ">Join
+                            Us</a>
+                    @endauth
+                </div>
+
             </div>
         </div>
     </nav>
